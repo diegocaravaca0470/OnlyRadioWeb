6e9e9afc75ebf8bf8812939162d041368c10cd12
diegocaravaca0470 <62868889+diegocaravaca0470@users.noreply.github.com>
2024-09-30 17:45:26 -0300

    Add files via upload

---
+++ WEB ONLY/index.js
@@ -0,0 +1,14 @@
+MRP.insert({
+    'url':'https://radio01.ferozo.com/proxy/ra01001056?mp=/stream',
+    'codec':'mp3',
+    'volume':100,
+    'autoplay':true,
+    'jsevents':true,
+    'buffering':0,
+    'title':'Only Radio',
+    'welcome':'Bienvenido.....',
+    'wmode':'transparent',
+    'skin':'listen-live',
+    'width':250,
+    'height':100
+    });
